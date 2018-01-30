9eab2e6dab95dce194600de652c0fb9cc3f199cd
MateuszKokot <33980659+MateuszKokot@users.noreply.github.com>
2018-01-31 00:26:11 +0100

    add control arrow

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 //Po wczytaniu plików odpal to
 $(document).ready(function(){
 
+mousePosition()
 wwSmallwwBig()
 scrollthumbList()
-mousePosition()
+moveThumbList()
 });
 
 
@@ -198,42 +199,7 @@ function mousePosition() {
 }
 
 
-// TO do przeanalizowania i zrobić z tego drag and dropea i slidera
- var ball = document.querySelector('.thumbList');
- ball.style('background-color','red');
-
-ball.onmousedown = function(event) {
-
-  let shiftX = event.clientX - ball.getBoundingClientRect().left;
-  let shiftY = event.clientY - ball.getBoundingClientRect().top;
-
-  ball.style.position = 'absolute';
-  ball.style.zIndex = 1000;
-  document.body.append(ball);
-
-  moveAt(event.pageX, event.pageY);
-
-  // centers the ball at (pageX, pageY) coordinates
-  function moveAt(pageX, pageY) {
-    ball.style.left = pageX - shiftX + 'px';
-    ball.style.top = pageY - shiftY + 'px';
-  }
-
-  function onMouseMove(event) {
-    moveAt(event.pageX, event.pageY);
-  }
-
-  // (3) move the ball on mousemove
-  document.addEventListener('mousemove', onMouseMove);
-
-  // (4) drop the ball, remove unneeded handlers
-  ball.onmouseup = function() {
-    document.removeEventListener('mousemove', onMouseMove);
-    ball.onmouseup = null;
-  };
-
-};
-
-ball.ondragstart = function() {
-  return false;
-};
+function moveThumbList() {
+ $('.thumbList').mousedown(function( event ) {
+  });
+}
